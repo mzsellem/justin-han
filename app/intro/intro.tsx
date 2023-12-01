4543f6e6eb679a63be8d7ec11de5b682eed578d2
mzsellem <marleysellem@gmail.com>
2023-12-01 12:34:21 -0500

    add navbar buttons, no function

--- app/intro/intro.tsx
+++ app/intro/intro.tsx
@@ -2,7 +2,7 @@ export default function Intro() {
   return (
    <>
     <main>
-        <h1 className="font-bold text-xl p-4 z-5 rounded-full bg-grey">JUSTIN HAN</h1>
+        <h1 className="font-bold text-xl p-4 z-5 rounded-md bg-grey">JUSTIN HAN</h1>
     </main>
    </>
   )
--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -2,10 +2,13 @@ export default function NavBar() {
     return (
         <>
             <div>
-                <div className="bg-bluesuit text-white h-screen w-full p-2">
-                    {/* Navbar content goes here */}
-                    <p className="md:text-lg text-sm font-bold">Navbar</p>
-                        {/* Add links or other navigation items */}
+                <div className="md:text-lg text-sm font-bold space-y-10 bg-bluesuit text-white h-screen w-full p-2 flex flex-col justify-center">
+                    <button>Navbar</button>
+                    <button>Bio</button>
+                    <button>Media</button>
+                    <button>Events</button>
+                    <button>Awards</button>
+                    <button>Contact</button>
                 </div>
             </div>
         </>
