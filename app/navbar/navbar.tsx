faeead430b608fc0106fd5ad64b790d35df84135
mzsellem <marleysellem@gmail.com>
2023-12-01 12:37:33 -0500

    adjust padding web/mobile

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -2,7 +2,7 @@ export default function NavBar() {
     return (
         <>
             <div>
-                <div className="md:text-lg text-sm font-bold space-y-10 bg-bluesuit text-white h-screen w-full p-2 flex flex-col justify-center">
+                <div className="md:text-lg text-sm font-bold space-y-10 bg-bluesuit text-white h-screen w-full md:p-4 p-2 flex flex-col justify-center">
                     <button>Navbar</button>
                     <button>Bio</button>
                     <button>Media</button>
