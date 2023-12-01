2f5dbd6bc074a9f0e16b087a71624c9474b1eb73
mzsellem <marleysellem@gmail.com>
2023-12-01 12:26:50 -0500

    left navbar and centered justin han

---
+++ app/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return (
+        <>
+            <div>
+                <div className="bg-bluesuit text-white h-screen w-full p-2">
+                    {/* Navbar content goes here */}
+                    <p className="md:text-lg text-sm font-bold">Navbar</p>
+                        {/* Add links or other navigation items */}
+                </div>
+            </div>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import profilePic from "../public/justin-park.png"
 import Intro from "./intro/intro"
+import NavBar from "./navbar/navbar"
 
 export default function Home() {
   return (
-   <>
     <>
-  <main className="relative flex flex-col justify-center items-end max-w-screen max-h-screen overflow-hidden">
-    <img
-      className="object-cover object-center w-full h-full"
-      src="/happy-justin.png"
-      alt="justin in a park"
-    />
-    <div className="absolute bottom-3/4 right-2/3 p-4 text-white bg-black text-4xl z-5">
-      <Intro />
+      <div className="flex">
+        <NavBar />
+        <div className="relative flex flex-col justify-center items-center max-w-screen max-h-screen overflow-hidden flex-1">
+          <img
+            className="object-cover object-center w-full h-full"
+            src="/happy-justin.png"
+            alt="justin in a park"
+          />
+          <div className="absolute md:right-2/3 top-0 mt-16">
+            <Intro />
+          </div>
+        </div>
     </div>
-  </main>
-</>
-
-   </>
+  </>
+  
   )
 }
