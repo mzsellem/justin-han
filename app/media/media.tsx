8c5ff3adb5e8d56bb33450195e95102c80025b4c
mzsellem <marleysellem@gmail.com>
2023-12-07 17:24:00 -0500

    better format and scrolling for media cards

--- app/media/media.tsx
+++ app/media/media.tsx
@@ -5,16 +5,18 @@ export default function PlaylistCard ({ title, videos }) {
     return (
         <>
             <div className="max-w-xs mx-auto bg-darksuit shadow-lg rounded-lg overflow-hidden">
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl text-white mb-2">{title}</div>
-                <ul>
-                {videos.map((video, index) => (
-                    <li key={index} className="text-white text-base mb-2">
-                    {video}
-                    </li>
-                ))}
-                </ul>
-            </div>
+                <div className="px-6 py-4">
+                    <div className="">
+                        <div className="font-bold text-xl text-white mb-2">{title}</div>
+                            <ul className="flex space-x-6 border p-4 overflow-y-auto">
+                                {videos.map((video, index) => (
+                                    <li key={index} className="text-base mb-2">
+                                    <div className='bg-gray-300 border'>{video}</div>
+                                    </li>
+                                ))}
+                            </ul>
+                    </div>
+                </div>
             </div>
         </>
     );
