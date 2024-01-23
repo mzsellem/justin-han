dcde3378562e9c4626b70e31b896c298fb1d57a2
mzsellem <marleysellem@gmail.com>
2024-01-23 10:35:46 -0500

    add helmet and configure

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+import "./middleware/csp.js";
+
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: csp.toString(),
+              },
+            ],
+          },
+        ];
+      },
+}
 
 module.exports = nextConfig
